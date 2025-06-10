f0174322e803516efba0091b60bfb629e136cc81
Once-1296 <awwab.wadekar@gmail.com>
2025-06-10 23:01:21 +0530

    Days 11 and 12 part 1 solved

---
+++ Day 11/part1.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include<set>
+#include<map>
+#include<queue>
+#include <string>
+typedef long long ll;
+using namespace std;
+typedef unsigned long long ull;
+ull dfs(int ops,string num,vector<map<ull,ull>>&dp)
+{
+    ull n = stoull(num);
+    //cout<<n<<" "<<num<<endl;
+    if(dp[ops].count(n))
+    {
+        return dp[ops][n];
+    }
+    if(ops==0)
+    {
+        return dp[ops][n]=1;
+    }
+    if(n==0ull)
+    {
+        return dp[ops][n]=dfs(ops-1,"1",dp);
+    }
+    if(num.length()%2==0)
+    {
+        int sz = num.length();
+        string lh = num.substr(0,sz/2),rh = num.substr(sz/2,sz/2);
+        while(rh.size()>1&&*rh.begin()=='0')rh.erase(rh.begin());
+        //cout<<rh<<endl;
+        return dp[ops][n]= dfs(ops-1,lh,dp)+dfs(ops-1,rh,dp);
+    }
+    n*=2024ull;
+    //cout<<n<<endl;
+    return dp[ops][n/2024]=dfs(ops-1,to_string(n),dp);
+}
+int main(){
+    fstream file("input.txt", ios::in);
+    string line;
+    ll ans=0;
+    vector<vector<char>>grid;
+    while (getline(file, line))
+    {
+        string word;
+        stringstream obj(line);
+        while(getline(obj,word,' '))
+        {
+            vector<map<ull,ull>>dp(26);
+            dfs(25,word,dp);
+            ans+=dp[25][stoull(word)];
+            //cout<<dp[1][stoull(word)]<<endl;
+        }
+    }
+
+    cout<<ans<<endl;
+}
---
+++ Day 12/part2.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include<set>
+#include<map>
+#include<queue>
+#include <string>
+typedef long long ll;
+using namespace std;
+typedef unsigned long long ull;
+const pair<int,int>dirs[4]={{1,0},{0,1},{-1,0},{0,-1}};
+map<pair<int,int>,map<int,set<pair<int,int>>>>cmap;
+map<pair<int,int>,map<int,set<pair<int,int>>>>rmap;
+class dsu
+{
+    public:
+    map<pair<int,int>,pair<int,int>>par;
+    map<pair<int,int>,ull>s2;
+    int sz;
+    dsu(vector<vector<char>>&gd)
+    {
+        int n = gd.size(),m =gd[0].size();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                rmap[{i,j}][i].insert({i,j});
+                cmap[{i,j}][j].insert({i,j});
+                par[{i,j}]={i,j};
+                s2[{i,j}]=1;
+                
+                //cout<<i<<" "<<j<<" ";
+                //cout<<s1[{i,j}]<<" "<<s2[{i,j}]<<endl;
+            }
+        }
+    }
+    pair<int,int> find(pair<int,int>u)
+    {
+        if(u!=par[u])return par[u]=find(par[u]);
+        return u;
+    }
+    void comb(pair<int,int> u, pair<int,int> v)
+    {
+        pair<int,int> ru = find(u),rv=find(v);
+        if(ru!=rv)
+        {
+            par[rv]=ru;
+            cmap[ru][rv.second].insert(rv);
+            rmap[ru][rv.first].insert(rv);
+            s2[ru]+=s2[rv];
+        }
+    }
+};
+int main(){
+    fstream file("input.txt", ios::in);
+    string line;
+    ll ans=0;
+    vector<vector<char>>grid;
+    while (getline(file, line))
+    {
+        vector<char>t(line.begin(),line.end());
+        grid.push_back(t);
+    }
+    dsu ds(grid);
+    int n= grid.size(),m= grid[0].size();
+    for(int i =0;i<n;i++)
+    {
+        for(int j =0;j<m;j++)
+        {
+            for(auto&e:dirs)
+                {
+                    int di = i+e.first,dj=j+e.second;
+                    if(di<0||di>=n||dj<0||dj>=m)
+                    {
+                        continue;
+                    }
+                    if(grid[i][j]==grid[di][dj])ds.comb({i,j},{di,dj});
+                }
+        }
+    }
+    vector<vector<bool>>vis(n,vector<bool>(m,0));
+    for(int i =0;i<n;i++)
+    {
+        for(int j =0;j<m;j++)
+        {
+            auto pr= ds.find({i,j});
+            if(!vis[pr.first][pr.second])
+            {
+                vis[pr.first][pr.second]=1;
+                for(auto&e:rmap[pr])
+                {
+                    int rn = e.first;
+                    int ct=0;
+                    bool b=0;
+                    for(auto&ee:e.second)
+                    {
+                       int i = ee.first,j=ee.second;
+                        
+                    }
+                }
+            }
+        }
+    }
+    cout<<ans<<endl;
+}
